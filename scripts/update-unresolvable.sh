4585764c9a28516db881d496c28ac1fe5a0e3150
realizelol <bsw.bsw@gmx.de>
2024-04-07 22:01:44 +0200

    [Workflow] Fix domain output. FIX

--- scripts/update-unresolvable.sh
+++ scripts/update-unresolvable.sh
@@ -6,7 +6,7 @@ content_lines="$(wc -l <unresolvable.txt)"
 sed_begin="$(seq "${param_cnt}" "50000" "${content_lines}")"
 sed_end="$(( $(seq "${param_cnt}" "50000" "${content_lines}" | tail -n1) + "49999" ))"
 
-if [ "${content_lines}" -lt "${sed_end}" ] && [ "${content_lines}" -gt "${sed_begin}" ]; then
+if [ "${param_cnt}" -lt "${sed_end}" ] && [ "${param_cnt}" -gt "${sed_begin}" ]; then
 
   echo "${sed_begin}" | while read -r sed_line; do
     sed -n "s/^\(.*\)#\([1-3]\)$/\1\t\2/p" unresolvable.txt | \
